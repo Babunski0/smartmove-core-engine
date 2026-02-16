755c79a26303061e9fb1880ad926d8423b72d325
Niyigaba82 <jna25001@student.mdu.se>
2026-02-16 16:02:08 +0100

    updated ApiResponse

--- src/main/java/com/smartmove/dto/response/ApiResponse.java
+++ src/main/java/com/smartmove/dto/response/ApiResponse.java
@@ -10,4 +10,12 @@ public class ApiResponse<T> {
     private boolean success;
     private String message;
     private T data;
+    private Long timestamp;
+
+    public ApiResponse(boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+        this.timestamp = System.currentTimeMillis();
+    }
 }
